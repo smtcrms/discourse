dadc7b8814455f2ca96d1ee9d49f663279b92091
Robin Ward <robin.ward@gmail.com>
2019-06-13 11:18:39 -0400

    UX: Rename "Keep Post" to "Keep Post Hidden" when hidden
    This is based on this feedback:
    https://meta.discourse.org/t/category-group-review-moderation/116478/19
    
    When a post is hidden this makes the operation much more clear.

--- app/models/reviewable_flagged_post.rb
+++ app/models/reviewable_flagged_post.rb
@@ -36,7 +36,12 @@ class ReviewableFlaggedPost < Reviewable
 
     agree = actions.add_bundle("#{id}-agree", icon: 'thumbs-up', label: 'reviewables.actions.agree.title')
 
-    build_action(actions, :agree_and_keep, icon: 'thumbs-up', bundle: agree)
+    if post.hidden?
+      build_action(actions, :agree_and_keep_hidden, icon: 'thumbs-up', bundle: agree)
+    else
+      build_action(actions, :agree_and_keep, icon: 'thumbs-up', bundle: agree)
+    end
+
     if guardian.can_suspend?(target_created_by)
       build_action(actions, :agree_and_suspend, icon: 'ban', bundle: agree, client_action: 'suspend')
       build_action(actions, :agree_and_silence, icon: 'microphone-slash', bundle: agree, client_action: 'silence')
@@ -119,6 +124,10 @@ class ReviewableFlaggedPost < Reviewable
 
   # Penalties are handled by the modal after the action is performed
   def perform_agree_and_keep(performed_by, args)
+    agree(performed_by, args)
+  end
+
+  def perform_agree_and_keep_hidden(performed_by, args)
     agree(performed_by, args)
   end
 
